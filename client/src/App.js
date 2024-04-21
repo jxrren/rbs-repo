4e2ce482c0cd4f3639f501ac8b4712fef339d277
jxrren <jarrenpng@gmail.com>
2024-04-22 00:58:06 +0800

    initial commit

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Room Booking System</h1>
+      <button className="btn btn-primary">bootstrap button</button>
+    </div>
+  );
+}
+
+export default App;
